5571ce6b2afd81168fb1469be90bc0afffe157a4
psatomas <psatomas@gmail.com>
2025-04-15 11:46:28 -0300

    feat: add the_unit_as_a_return_type.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let result = square(5);
-    println!("The square of 5 is {result}");
-
-    let result = square(13);
-    println!("The square of 13 is {result}");
-}
-
-fn square(number: i32) -> i32 {
-  number * number
+  
 }
